1b5f409f85907800fc303d992932f51d35995aac
shortxmas <leim56@hawaii.rr.com>
2023-09-04 13:29:08 -1000

    simplified about me on home page

--- src/Views/Home.tsx
+++ src/Views/Home.tsx
@@ -1,10 +1,13 @@
 import { HomeButtons } from '../Components/HomeButtons'
 import portrait from '../assets/test.png'
-
+import { useState } from 'react'
 
 
 export function Home(){
     document.title = "Home"
+    const [info,showInfo] = useState('none')
+    const [moreButton,changeMoreButton] = useState('')
+    const [lessButton,changeLessButton] = useState('none')
 
     return(
         <>
@@ -53,45 +56,69 @@ export function Home(){
             className="row justify-content-center"
             style={{ textAlign: "center", paddingTop: 50 }}
             >
-            <div className="col-lg-9 col-12">
+            <div className="col-lg-8 col-12">
                 <div className="box">
                 <div
-                    className="card-body text-bg-dark p-3"
-                    style={{ borderRadius: 20 }}
+                    className="card-body text-bg-light p-3"
+                    style={{ borderRadius: 20,boxShadow: 'rgba(0, 0, 0, 0.35) 0px 5px 15px'}}
+
                 >
-                    <h2 className="card-title" style={{ color: "#B2DFBF" }}>
+                    <h2 className="card-title" style={{ color: '#137a63' }}>
                     About Me!
                     </h2>
-                    <br />
-                    <h5 style={{ color: "lightgray" }}>
-                    <p id="aboutmep1">
-                        Hello! My name is Leighton Miguel, I am currently 19 years old,
-                        my favorite color is mint green, and I love technology! I am
-                        from a homey and lush town called Mililani which is on the
-                        island of Oahu and a part of the beautiful state of Hawaii. I
-                        grew up playing tennis, traveling frequently, playing video
-                        games and most importantly, building computers! I have been
-                        surrounded by computers and technology for all of my early life
-                        as my dad built his own computers as a side hobby! My upbringing
-                        in a loving family with a love for computers has fostered my
-                        deep appreciation for them and shaped my desire to further my
-                        skills in programming!
-                    </p>
+                    <div style={{display:info}}>
+                        <br />
+                        <h5 style={{ color: '#2F4F4F',fontWeight:'400'}}>
+                        <p id="aboutmep1">
+                            Hello! My name is Leighton Miguel, I am currently 19 years old,
+                            my favorite color is mint green, and I love technology! I am
+                            from a homey and lush town called Mililani which is on the
+                            island of Oahu and a part of the beautiful state of Hawaii. I
+                            grew up playing tennis, traveling frequently, playing video
+                            games and building computers. I have been
+                            surrounded by computers and technology for all of my early life
+                            as my dad built his own computers as a side hobby. My upbringing
+                            in a loving family with a love for computers has fostered my
+                            deep appreciation for them and shaped my desire to further my
+                            skills in programming!
+                        </p>
+                        
+                        
+                        <p id="aboutmep2">
+                            {" "}
+                            My love for Hawaii as a home and as a place ultimately helped me
+                            decide to attend the University of Hawaii at Manoa, and with my
+                            love for technology and computers, it was a no brainer for me to
+                            major in Computer Science. At the time of writing this, I am currently a sophomore and
+                            anticipated to graduate in the Spring of
+                            2026. I happily look forward to the adventures I know will
+                            come my way!
+                        </p>
+                        </h5>
+                    </div>
                     
-                    
-                    <p id="aboutmep2">
-                        {" "}
-                        My love for Hawaii as a home and as a place ultimately helped me
-                        decide to attend the Universit y of Hawaii at Manoa, and with my
-                        love for technology and computers, it was a no brainer for me to
-                        major in Computer Science! At the time of the creation of this
-                        website, I just finished my freshman year and am heading into my
-                        sophomore year. I am anticipated to graduate in the Spring of
-                        2026 and I happily look forward to the adventures I know will
-                        come my way!
-                    </p>
-                    </h5>
+                    <div className="pt-1">
+                        <button type="button" className="btn btn-success " style={{backgroundColor:'#137a63',display:lessButton}}
+                        onClick={() => {
+                            showInfo('none');
+                            changeMoreButton('');
+                            changeLessButton('none');
+
+                        }}>Collapse</button>
+                    </div>
+
+                    <div className="pt-1">
+                        <button type="button" className="btn btn-success " style={{backgroundColor:'#137a63',display:moreButton}}
+                        onClick={() =>{
+                            showInfo('');
+                            changeMoreButton('none');
+                            changeLessButton('');
+                        }}>Expand</button>
+                    </div>
+
+
                 </div>
+                
                 </div>
             </div>
             </div>
